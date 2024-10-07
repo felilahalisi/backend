4873cffcc47db377619491acd6423ff52c04cbbf
felilahalisi <161546383+felilahalisi@users.noreply.github.com>
2024-10-07 15:16:14 +0700

    Create Tugas Pertemuan 2_1.py

---
+++ Tugas Pertemuan 2_1.py
@@ -0,0 +1,6 @@
+total_belanja = int(input("Masukkan Total Belanja Anda = "))
+
+if total_belanja > 100000:
+    print("Selamat, Anda dapat hadiah!")
+else :
+    print("Terimakasih sudah berbelanja")
