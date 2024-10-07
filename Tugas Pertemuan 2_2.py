df0462612077f120ada5d93e313e64a9b782935a
felilahalisi <161546383+felilahalisi@users.noreply.github.com>
2024-10-07 15:16:49 +0700

    Create Tugas Pertemuan 2_2.py

---
+++ Tugas Pertemuan 2_2.py
@@ -0,0 +1,8 @@
+password = input("Masukkan Password Anda = ")
+
+if password == "Feli":
+    print("Selamat datang bos!")
+else:
+    print("Password salah, coba lagi")
+
+print("Terimakasih sudah menggunakan aplikasi kami")
